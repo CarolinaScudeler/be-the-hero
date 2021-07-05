ae5902a9489cefc468d6ceb2556454fdac381623
Carolina Scudeler <carolina.oliveira@bandtec.com.br>
2021-07-05 06:12:58 -0300

    Complete project

---
+++ README.md
@@ -0,0 +1,2 @@
+# be-the-hero
+Projeto Be The Hero realizado na Semana OmniStack 11
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./global.css";
+
+import Routes from "./routes";
+
+// JSX (JavaScript XML): HTML integrado dentro do JavaScript
+
+function App() {
+  return <Routes />;
+}
+
+export default App;
